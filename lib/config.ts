104b020f8ff781b606d36934c203a4a25b43556b
JOONSANG PARK <joonsangpark@JOONSANGui-MacBookPro.local>
2026-02-09 15:17:42 +0900

    chore: update author name and email

--- lib/config.ts
+++ lib/config.ts
@@ -3,8 +3,8 @@ export const siteConfig = {
   title: 'Joonit',
   description: '개발, 기술, 그리고 일상에 대한 이야기를 나누는 개인 블로그입니다.',
   author: {
-    name: 'Joonsang',
-    email: 'parkjs1362@kakao.com',
+    name: 'Joonit',
+    email: 'joonit1362@gmail.com',
     github: 'https://github.com/parkjs1362',
   },
   keywords: ['블로그', '개발', '프로그래밍', 'Next.js', 'React', 'TypeScript'] as string[],
